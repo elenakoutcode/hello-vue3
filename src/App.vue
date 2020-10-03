31ee7d3277039ed495e7c47a7a99c04c902cec13
elenakout <elenakout@gmail.com>
2020-10-03 13:16:38 +0300

    [dev] date picker component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <h1>hello vue 3</h1>
+  <div class="container">
+    <h1 class="pageTitle">Date Picker</h1>
+    <date-picker></date-picker>
+  </div>
 </template>
 
 <script>
+// import { ref, onMounted } from 'vue';
+
+import DatePicker from '@/components/DatePicker';
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    DatePicker,
+  },
+  setup() {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+
+    const startingYear = 2010;
+    const numberofYears = 20;
+
+    return {
+      months,
+      numberofYears,
+      startingYear,
+    };
+  },
 };
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
 *,
 *::before,
 *::after {
@@ -24,7 +60,23 @@ export default {
 }
 
 html {
+  font-family: 'Roboto', sans-serif;
   font-size: 10px;
   color: #1e1e1e;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.pageTitle {
+  padding: 20px 40px;
+  margin-bottom: 42px;
+  background-color: rgb(54, 54, 161);
+  color: #f1f1f1;
 }
 </style>
